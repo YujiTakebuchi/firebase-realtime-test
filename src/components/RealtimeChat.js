902ce9f0879f80a4d4015d8f28207f0f90308635
YujiTakebuchi <takeyou.swift@gmail.com>
2023-06-23 16:56:26 +0900

    データ登録処理追加

--- src/components/RealtimeChat.js
+++ src/components/RealtimeChat.js
@@ -1,15 +1,48 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
+import { useState } from "react";
+
+const env = process.env;
+
+const firebaseConfig = {
+  databaseURL: env.REACT_APP_FIREBASE_DB_URL,
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 export const ChatInputField = () => {
+  const [nameStat, setNameStat] = useState("");
+  const [messageStat, setMessageStat] = useState("");
+
+  const room = "chat_room";
+  const handleSendButton = () => {
+    set(ref(database, room), {
+      name: nameStat,
+      message: messageStat,
+    });
+    setNameStat("");
+    setMessageStat("");
+  };
   return (
     <div>
       <div>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={nameStat}
+          onChange={(e) => setNameStat(e.currentTarget.value)}
+        />
       </div>
       <div>
-        <textarea id="message" row="10"></textarea>
-        <button id="send">send</button>
+        <textarea
+          id="message"
+          row="10"
+          value={messageStat}
+          onChange={(e) => setMessageStat(e.currentTarget.value)}
+        ></textarea>
+        <button id="send" onClick={handleSendButton}>
+          send
+        </button>
       </div>
       <div id="output"></div>
     </div>
@@ -17,21 +50,6 @@ export const ChatInputField = () => {
 };
 
 const RealtimeChat = () => {
-  const env = process.env;
-
-  const connectRealtimeDatabase = () => {
-    console.log(process.env.REACT_APP_FIREBASE_DB_URL);
-    const firebaseConfig = {
-      databaseURL: env.REACT_APP_FIREBASE_DB_URL,
-    };
-
-    const app = initializeApp(firebaseConfig);
-
-    const database = getDatabase(app);
-    console.log(database);
-  };
-
-  connectRealtimeDatabase();
   return <ChatInputField />;
 };
 
