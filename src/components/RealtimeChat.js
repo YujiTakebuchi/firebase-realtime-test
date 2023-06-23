8269eb14b9fb4a3a9ff76c2192d9c7198bcd06dc
YujiTakebuchi <takeyou.swift@gmail.com>
2023-06-23 16:18:16 +0900

    RealtimeDatabase情報取得、dotenvがcreate-react-appでデフォルトで用意されてたので削除

--- src/components/RealtimeChat.js
+++ src/components/RealtimeChat.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
 export const ChatInputField = () => {
   return (
     <div>
@@ -14,6 +17,21 @@ export const ChatInputField = () => {
 };
 
 const RealtimeChat = () => {
+  const env = process.env;
+
+  const connectRealtimeDatabase = () => {
+    console.log(process.env.REACT_APP_FIREBASE_DB_URL);
+    const firebaseConfig = {
+      databaseURL: env.REACT_APP_FIREBASE_DB_URL,
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    const database = getDatabase(app);
+    console.log(database);
+  };
+
+  connectRealtimeDatabase();
   return <ChatInputField />;
 };
 
