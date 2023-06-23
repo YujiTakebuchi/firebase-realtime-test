e8cbdb7e8d9717958b65f141e71a8642b5058fce
YujiTakebuchi <takeyou.swift@gmail.com>
2023-06-23 19:49:39 +0900

    送信時メッセージ追加のビュー側処理追加

--- src/components/RealtimeChat.js
+++ src/components/RealtimeChat.js
@@ -1,6 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue } from "firebase/database";
-import { useEffect, useState } from "react";
+import {
+  getDatabase,
+  ref,
+  push,
+  onChildAdded,
+  child,
+  get,
+} from "firebase/database";
+import { useEffect, useReducer, useRef, useState } from "react";
 
 const env = process.env;
 
@@ -10,25 +17,45 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+const reducerFunc = (msgList, action) => {
+  switch (action.type) {
+    case "push":
+      return [...msgList].concat(action.messageList);
+  }
+};
+
 export const ChatInputField = () => {
   const [nameStat, setNameStat] = useState("");
   const [messageStat, setMessageStat] = useState("");
-  const [receivedMessageList, setReceivedMessageList] = useState([]);
+  const [receivedMessageList, setReceivedMessageList] = useReducer(
+    reducerFunc,
+    []
+  );
+  const refKeyRef = useRef(null);
 
   const room = "chat_room";
   const handleSendButton = () => {
-    set(ref(database, room), {
+    const pushObj = push(ref(database, room), {
       name: nameStat,
       message: messageStat,
     });
+    refKeyRef.current = pushObj.key;
     setNameStat("");
     setMessageStat("");
   };
   const connectChatDb = () => {
-    onValue(ref(database, room), (data) => {
+    // 過去のメッセージ取得
+    get(child(ref(database), `${room}`)).then((data) => {
+      const v = data.val();
+      const messageList = Object.keys(v).map((m) => v[m]);
+      setReceivedMessageList({ type: "push", messageList: messageList });
+    });
+
+    // メッセージ送信時のリスナ登録
+    onChildAdded(ref(database, room), (data) => {
       const v = data.val();
       const k = data.key;
-      setReceivedMessageList([...receivedMessageList, v]);
+      setReceivedMessageList({ type: "push", messageList: [v] });
     });
   };
 
@@ -56,6 +83,7 @@ export const ChatInputField = () => {
       </div>
       <div id="output">
         {receivedMessageList.map((rm) => {
+          if (!rm) return;
           return (
             <>
               <div className="name">{`名前: ${rm.name}`}</div>
